4048e1991c5596ad6b7afc0969b675ac6bfede97
Dirtsam <67219931+Dirtsam1@users.noreply.github.com>
2021-02-06 20:43:22 +0530

    Add files via upload
    A chat app that makes teams life easy

---
+++ app.js
@@ -0,0 +1,11 @@
+/* Open */
+function openNav() {
+  document.getElementById("topNav").style.height = "60%";
+}
+
+/* Close */
+function closeNav() {
+  document.getElementById("topNav").style.height = "0%";
+}
+
+let menu = document.querySelector(".hamburger");
